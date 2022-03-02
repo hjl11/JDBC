b470abedda33b8b298c6f79526f3648bc7fb6dda
hejinling0323 <hjl980323@163.com>
2022-03-02 17:52:11 +0800

    first commit

---
+++ day02/src/hejinling/exer1/GitTest.java
@@ -0,0 +1,11 @@
+package hejinling.exer1;
+
+/**
+ * @author hejinling
+ * @create 2022-03-02 17:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!1");
+    }
+}
