14eb7d22483971fce4020c41ea9a6f82803ec6a2
hejinling0323 <hjl980323@163.com>
2022-03-02 17:52:57 +0800

    second commit

--- day02/src/hejinling/exer1/GitTest.java
+++ day02/src/hejinling/exer1/GitTest.java
@@ -7,5 +7,6 @@ package hejinling.exer1;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!1");
+        System.out.println("hello world!2");
     }
 }
